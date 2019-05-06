8da25047cb94c33757f23a3ac4b747683d80206d
itosho <sho.ito@connehito.com>
2019-05-06 14:15:18 +0900

    Fix font bug

--- nlp/visualize.py
+++ nlp/visualize.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
 # python visualize.py jawiki_word2vec.bin word2vec_visualization.png
 
+# notice: https://stackoverflow.com/questions/21784641/installation-issue-with-matplotlib-python
 import sys
 import matplotlib.pyplot as plt
 import numpy as np
 from gensim.models import Word2Vec
 from sklearn.manifold import TSNE
 
+# obtained https://github.com/ghmagazine/python_ml_book/blob/master/01/matplotlib_japanize/matplotlib_japanize.md
+plt.rcParams["font.family"] = "IPAexGothic"
+
 model = Word2Vec.load(sys.argv[1])
 
-words = [word for (word, score) in model.wv.most_similar('砂糖', topn=50)]
-words.append('砂糖')
+words = [word for (word, score) in model.wv.most_similar('錦糸町', topn=50)]
+words.append('錦糸町')
 vectors = np.vstack([model[word] for word in words])
 
 tsne = TSNE(n_components=2)
